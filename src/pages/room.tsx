bea7da8a8288773ebcdca8c484e0de8dd60bddef
Stefan Schneider <stefanguigo@gmail.com>
2025-07-09 14:52:01 -0300

    feat: initial room functions

---
+++ src/pages/room.tsx
@@ -0,0 +1,19 @@
+import { Navigate, useParams } from 'react-router-dom';
+
+type RoomParams = {
+  id: string;
+};
+
+export function Room() {
+  const params = useParams<RoomParams>();
+
+  if (!params.id) {
+    return <Navigate replace to="/" />;
+  }
+
+  return (
+    <div>
+      <h1>Room Details</h1>
+    </div>
+  );
+}
